3d96c564c60e3963928097aa3d577ddf4d492559
Vladyslav Veretilnyk <vvveretil@gmail.com>
2022-11-09 17:29:25 +0200

    added profile page

--- src/main/java/com/vladveretilnyk/workshop/config/Page.java
+++ src/main/java/com/vladveretilnyk/workshop/config/Page.java
@@ -5,6 +5,8 @@ public class Page {
     public static final String LOGIN_PAGE = "login";
     public static final String REGISTRATION_PAGE = "registration";
 
+    public static final String PROFILE_PAGE = "profile/profile";
+
     public static final String REDIRECT_LOGIN_PAGE = "redirect:/" + LOGIN_PAGE;
 
 }
--- src/main/java/com/vladveretilnyk/workshop/user/UserController.java
+++ src/main/java/com/vladveretilnyk/workshop/user/UserController.java
@@ -1,4 +1,18 @@
 package com.vladveretilnyk.workshop.user;
 
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import static com.vladveretilnyk.workshop.config.Page.PROFILE_PAGE;
+
+@Controller
+@AllArgsConstructor
 public class UserController {
+
+    @GetMapping("/profile")
+    public String getProfilePage() {
+        return PROFILE_PAGE;
+    }
+
 }
